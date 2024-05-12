22df5c5faa934aac270e7c26b80d2f672185e17a
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-05-12 11:06:45 -0300

    12/05 11:06

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -155,6 +155,8 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -172,6 +174,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
